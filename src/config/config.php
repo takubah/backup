6512619ec1f961600a0db8481508647e3ecd3c20
kiddcode <akiddcode@gmail.com>
2014-06-17 22:28:10 +0700

    autoload seeders in composer.json and add config.php

---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	'profiler'		=> Config::get('app.debug'),
+
+	/**
+	* Set Database
+	*/
+	'database'		=> 'mysql',
+
+	'auth_model'	=> 'Antoniputra\Asmoyo\Users\User',
+
+);
