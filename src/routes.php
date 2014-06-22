3e15ca1c880764842de32255a1f4736eb35e8a85
kiddcode <akiddcode@gmail.com>
2014-06-22 21:10:08 +0700

    update routes

--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,30 @@
 <?php
 
-$adminUrl = Config::get('asmoyo::admin_url');
-
 /* Route Pattern */
 Route::pattern('id', '[0-9]+');
 Route::pattern('slug', '[A-Za-z0-9-_]+');
 /* End Route Pattern */
 
+
+/**
+* API ROUTING
+*/
+	$apiUrl 	= Config::get('asmoyo::api.url');
+
+	Route::group(array('prefix' => $apiUrl, 'before' => 'ApiFilter'), function() use($apiUrl)
+	{
+		// ApiUser
+		Route::resource('users', 'Api_UserController');
+	});
+/**
+* End API ROUTING
+*/
+
+
 /**
 * ADMIN ROUTING
 */
+	$adminUrl 	= Config::get('asmoyo::admin.url');
 
 	// Authentication
 	Route::group(array('prefix' => $adminUrl), function() use($adminUrl)
@@ -17,16 +32,16 @@ Route::pattern('slug', '[A-Za-z0-9-_]+');
 		Route::get('login', array(
 			'before'	=> 'Anonymous',
 			'as' 		=> 'admin.auth.login',
-			'uses' 		=> 'UserController@adminLogin'
+			'uses' 		=> 'Admin_UserController@adminLogin'
 		));
 		Route::post('login', array(
 			'before'	=> 'Anonymous',
 			'as' 		=> 'admin.auth.login',
-			'uses' 		=> 'UserController@postAdminLogin'
+			'uses' 		=> 'Admin_UserController@postAdminLogin'
 		));
 		Route::get('logout', array(
 			'as' 		=> 'admin.auth.logout',
-			'uses' 		=> 'UserController@adminLogout'
+			'uses' 		=> 'Admin_UserController@adminLogout'
 		));
 	});
 	// End Authentication 
@@ -34,7 +49,7 @@ Route::pattern('slug', '[A-Za-z0-9-_]+');
 	Route::group(array('prefix' => $adminUrl, 'before' => 'AdminFilter'), function() use($adminUrl)
 	{
 		// user
-		Route::resource('user', 'UserController');
+		Route::resource('user', 'Admin_UserController');
 	});
 
 /**
