73bb4c2eff48c1bdc12567039b164a9b8d744648
kiddcode <akiddcode@gmail.com>
2014-06-25 02:27:34 +0700

    add repo Option get for global (bindShared)

--- src/Antoniputra/Asmoyo/Options/OptionRepo.php
+++ src/Antoniputra/Asmoyo/Options/OptionRepo.php
@@ -1,5 +1,6 @@
 <?php namespace Antoniputra\Asmoyo\Options;
 
+use Config, Cache;
 use Antoniputra\Asmoyo\Cores\RepoBase;
 
 class OptionRepo extends RepoBase {
@@ -9,4 +10,36 @@ class OptionRepo extends RepoBase {
 		$this->model = $model;
 	}
 
+	public function get($key=null)
+	{
+		$cacheBaseKey	= Config::get('asmoyo::config.cache.baseName');
+		$cacheKey		= 'asmoyo.web';
+
+		// check cache
+		if( Cache::tags( $cacheBaseKey )->has($cacheKey) )
+		{
+			$optionCached = Cache::tags( $cacheBaseKey )->get($cacheKey);
+
+			return ( !$key ) ? $optionCached : $optionCached[$key];
+		}
+
+		$result = array();
+		foreach( \Antoniputra\Asmoyo\Options\Option::all() as $opt )
+		{
+			if($opt['type'] == 'json')
+			{
+				$result[$opt['name']]	= json_decode($opt['value'], true);
+			} else {
+				$result[$opt['name']]	= $opt['value'];
+			}
+		}
+
+		// save item to cache
+		$optionData	= Cache::tags( $cacheBaseKey )->rememberForever($cacheKey, function() use ($result)
+		{
+			return $result;
+		});
+		
+		return ( !$key ) ? $optionData : $optionData[$key];
+	}
 }
