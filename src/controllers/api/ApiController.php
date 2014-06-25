9ae743202a2000e91095bf38a199db28be155273
kiddcode <akiddcode@gmail.com>
2014-06-25 10:46:10 +0700

    add api controller parent

---
+++ src/controllers/api/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+class ApiController extends \Controller
+{
+	public function __construct()
+	{
+		
+	}
+
+	public function missingMethod($parameters = array())
+	{
+		return 'not found prends';
+	}
+}
