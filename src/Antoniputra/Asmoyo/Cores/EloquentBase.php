9feb9a1c263f42ceca16eb7e768cbf7f143d7c42
kiddcode <akiddcode@gmail.com>
2014-06-29 03:04:54 +0700

    add method myMorphTo for polymorph relation support namespace

--- src/Antoniputra/Asmoyo/Cores/EloquentBase.php
+++ src/Antoniputra/Asmoyo/Cores/EloquentBase.php
@@ -1,5 +1,7 @@
 <?php namespace Antoniputra\Asmoyo\Cores;
 
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
 class EloquentBase extends \Eloquent
 {
 	
@@ -11,6 +13,34 @@ class EloquentBase extends \Eloquent
 	public function getFillable($fillable=array())
 	{
 		return $this->fillable;
+	}
+
+	public function myMorphTo($related = null, $name = null, $type = null, $id = null)
+	{
+		if (is_null($name))
+		{
+			list(, $caller) = debug_backtrace(false);
+
+			$name = snake_case($caller['function']);
+		}
+
+		list($type, $id) = $this->getMorphs($name, $type, $id);
+
+		if (is_null($class = $this->$type))
+		{
+			return new MorphTo(
+				$this->newQuery(), $this, $id, null, $type, $name
+			);
+		}
+
+		else
+		{
+			$instance = ($related) ? new $related : new $class;
+
+			return new MorphTo(
+				with($instance)->newQuery(), $this, $id, $instance->getKeyName(), $type, $name
+			);
+		}
 	}
 
 }
