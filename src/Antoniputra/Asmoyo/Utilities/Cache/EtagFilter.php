9d6b4572c9643fbb74296f2ed8dd3f5442b93ec9
kiddcode <akiddcode@gmail.com>
2014-07-04 15:19:10 +0700

    fix Etag Filter

--- src/Antoniputra/Asmoyo/Utilities/Cache/EtagFilter.php
+++ src/Antoniputra/Asmoyo/Utilities/Cache/EtagFilter.php
@@ -3,25 +3,66 @@
 use Illuminate\Routing\Route;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Config;
+use App, Config;
 
 class EtagFilter {
 	
 	public function get(Route $route, Request $request)
 	{
-
+		$entity = $this->entity( $request->path() );
+		
+		if( $this->isValid( $entity, $request->getEtags() ) )
+		{
+			return App::abort(304);
+		}
 	}
 
 	public function set(Route $route, Request $request, Response $response)
 	{
-
+		$entity = $this->entity( $request->path() );
+ 
+	 	$response->setEtag( md5($entity) );
 	}
 
-	protected function makeKey($url)
+	protected function entity($url)
 	{
-		$web = app('asmoyo.web');
-		public_path('packages/antoniputra/asmoyo/'. $web['web_adminTemplate'].'/'.$file);
+		if( false !== strpos($url, 'admin') )
+		{
+			$path = public_path( str_replace('assets/admin', 'packages/antoniputra/asmoyo/admin', $url) );
+		} else {
+			$path = '';
+		}
 
-		return 
+		if( file_exists($path) )
+		{
+			return filemtime($path);
+		}
+	}
+
+	/**
+	* check if between given key and last key valid
+	* @param integer $entity (contain file time)
+	* @param current Etags key
+	*/
+	private function isValid($entity, $etag)
+	{
+		$entity = md5($entity);
+		$etag 	= str_replace('"', '', $etag);
+
+		if ( isset($etag[0]) )
+		{
+			if ( $etag[0] === $entity )
+	    	{
+		        return true;
+	    	}
+
+	    	// etag gzip
+	    	$e_gzip = $entity.'-gzip';
+	    	
+	    	if( $e_gzip === $etag[0] )
+	    	{
+	    		return true;
+	    	}
+    	}
 	}
 }
