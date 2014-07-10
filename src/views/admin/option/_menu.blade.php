8afa52d9d7091cb816cff5484f5d47d6d2a5eaf2
kiddcode <akiddcode@gmail.com>
2014-07-10 15:33:10 +0700

    update option views

---
+++ src/views/admin/option/_menu.blade.php
@@ -0,0 +1,17 @@
+<div style="margin-bottom:10px;">
+	<ul class="nav nav-pills asmoyo-nav-pills">
+		<li>
+			<a href="{{route('admin.option.web')}}">
+				<i class="fa fa-globe"></i>
+				Web
+			</a>
+		</li>
+		<li>
+			<a href="{{route('admin.option.media')}}">
+				<i class="fa fa-picture-o"></i>
+				Media
+			</a>
+		</li>
+	</ul>
+</div>
+<hr>
