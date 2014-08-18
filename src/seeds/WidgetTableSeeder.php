99ad9c633e981f6337ea01db5fa6acf9a0f4fd80
toniputra <akiddcode@gmail.com>
2014-08-19 02:23:36 +0700

    update seeder widget : Text

--- src/seeds/WidgetTableSeeder.php
+++ src/seeds/WidgetTableSeeder.php
@@ -17,7 +17,6 @@ class WidgetTableSeeder extends Seeder {
 			    'attribute'		=> json_encode(array(
 			    	'field' => array(
 		    			'text'		=> 'textarea',
-		    			'is_paragraph' => 'boolean',
 	    			),
 		    	)),
 			),
@@ -59,13 +58,12 @@ class WidgetTableSeeder extends Seeder {
 				'title'			=> 'Informasi',
 				'description'	=> 'ini adalah informasi saja',
 				'content'		=> json_encode(array(
-					'text'	=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-					'is_paragraph'	=> true,
+					'text'	=> nl2br('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
 				)),
 			),
 			array(
