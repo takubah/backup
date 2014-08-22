99ad44410d71a943bea494656ff83ccb8f40eb86
toniputra <akiddcode@gmail.com>
2014-08-23 01:36:25 +0700

    update widget for banner

--- partials/banner.blade.php
+++ partials/banner.blade.php
@@ -1,35 +1,5 @@
 <br><br>
 
 <div class="banner">
-	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner">
-			<div class="item active">
-				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNTU1O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+" alt="" style="width:100%;">
-				<div class="carousel-caption">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</div>
-			<div class="item">
-				<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNDQ0O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==" alt="" style="width:100%;">
-				<div class="carousel-caption">
-					jiffie duke derided
-				</div>
-			</div>
-		</div>
-
-		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-		</a>
-	</div>
+	{{ '{<asmoyo:widget name=banner item=3>}' }}
 </div>
--- pseudo/widget.blade.php
+++ pseudo/widget.blade.php
@@ -24,4 +24,42 @@
 		{{Form::text('keyword', null, array('class' => 'form-control', 'placeholder' => 'Keyword..'))}}
 	</form>
 
+@elseif( $attr['name'] == 'banner' )
+	
+	@if($widget['item']['content'])
+		<div id="banner_{{$widget['item']['id']}}" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<?php $i = 0; ?>
+				@foreach($widget['item']['content'] as $c)
+					<li data-target="#banner_{{$widget['item']['id']}}" data-slide-to="{{$i}}" class="@if($i==0) active @endif"></li>
+				<?php $i++; ?>
+				@endforeach
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<?php $i = 0; ?>
+				@foreach($widget['item']['content'] as $c)
+					<div class="item @if($i==0) active @endif">
+						<img src="{{getMedia($c['file'])}}" style="width:100%;">
+						<div class="carousel-caption">
+							<h3>{{$c['title']}}</h3>
+							<p>{{$c['description']}}</p>
+						</div>
+					</div>
+					<?php $i++; ?>
+				@endforeach
+			</div>
+
+			<!-- Controls -->
+			<a class="left carousel-control" href="#banner_{{$widget['item']['id']}}" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			</a>
+			<a class="right carousel-control" href="#banner_{{$widget['item']['id']}}" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+	@endif
+
 @endif
